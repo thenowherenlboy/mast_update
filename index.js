9214efaf4bfb681b6d629b5bd61fe4d9853de949
Dickson <dicksonh16@students.ecu.edu>
2019-08-21 10:47:05 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// MAST update attempt
+
+/* Project goals:
+    Update MAST backend to Node.js
+        -Header and Footer files to be more consistent across site
+            (Current footer for Module Selection page do not point to production URLs)
+    Update Look And Feel of the frontend (Maybe something a bit more purple and gold?)
+    Update Module Selection page with iBooks column
+    Other tasks as assigned by AAH COE Office of Assessment, Data Management, and Digital Learning
+*/
+
